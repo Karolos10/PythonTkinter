97d0b2dfab0b0153470adaed0aa07aa67a207432
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-09-17 19:40:05 -0500

    tkinter practice

---
+++ interface.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+raiz = Tk()
+
+miFrame = Frame(raiz, width=1200, height=600)
+miFrame.pack()
+
+cuadroNombre = Entry(miFrame)
+cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red")
+
+cuadroPass = Entry(miFrame)
+cuadroPass.grid(row=1, column=1, padx=10, pady=10)
+cuadroPass.config(show="*")
+
+cuadroApellido = Entry(miFrame)
+cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
+
+cuadroDireccion = Entry(miFrame)
+cuadroDireccion.grid(row=3, column=1, padx=10, pady=10)
+
+nombreLabel = Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=0, column=0, sticky="w", padx=10, pady=10)
+
+passLabel = Label(miFrame, text="Contraseña:")
+passLabel.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+
+apellidoLabel = Label(miFrame, text="Apellido:")
+apellidoLabel.grid(row=2, column=0, sticky="w", padx=10, pady=10)
+
+direccionLabel = Label(miFrame, text="Dirección:")
+direccionLabel.grid(row=3, column=0, sticky="w", padx=10, pady=10)
+
+raiz.mainloop()
